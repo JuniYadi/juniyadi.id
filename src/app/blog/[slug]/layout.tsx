cac9b003717b867d1e91feb5212ee309666bfc03
JuniYadi <juniyadi94@gmail.com>
2025-03-15 22:02:31 +0700

    Update Blog Layout

--- src/app/blog/[slug]/layout.tsx
+++ src/app/blog/[slug]/layout.tsx
@@ -10,26 +10,14 @@ interface BlogPostLayoutProps {
 export default function BlogPostLayout({ children }: BlogPostLayoutProps) {
   return (
     <div className="container mx-auto px-4 py-8">
-      <div className="grid grid-cols-1 lg:grid-cols-12 gap-8">
-        {/* Main content - blog post */}
-        <main className="lg:col-span-9 prose prose-lg dark:prose-invert max-w-none">
+      <div className="grid grid-cols-1 gap-8">
+        {/* Main content - blog post - now full width */}
+        <main className="prose prose-lg dark:prose-invert max-w-none">
           {children}
         </main>
 
-        {/* Right sidebar - Table of Contents */}
-        <aside className="lg:col-span-3 hidden lg:block">
-          {/* <div className="sticky top-20">
-            <h3 className="font-bold text-lg mb-4">Table of Contents</h3>
-            <div id="table-of-contents" className="text-sm">
-              TABLE_OF_CONTENTS
-            </div>
-          </div> */}
-          TABLE_OF_CONTENTS
-        </aside>
-
-        {/* Comments section - spans full width below the content */}
-        <div className="lg:col-span-12 mt-12 pt-8 border-t">
-          <h3 className="font-bold text-xl mb-6">Comments</h3>
+        {/* Comments section - below the content */}
+        <div className="mt-12 pt-8 border-t">
           <Comments />
         </div>
       </div>
