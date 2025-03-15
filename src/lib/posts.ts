d1ffeb0d7dd0873aed349ee3d25ce981e1847ba7
JuniYadi <juniyadi94@gmail.com>
2025-03-16 04:23:21 +0700

    feat: Add function to group posts by specified object key for enhanced content organization

--- src/lib/posts.ts
+++ src/lib/posts.ts
@@ -1,4 +1,5 @@
 import FusionCollection from "fusionable/FusionCollection";
+import { s } from "./slug";
 
 export const getContent = () => {
   const collection = new FusionCollection()
@@ -17,4 +18,53 @@ export const getAllPosts = (limit?: number) => {
 
 export const getPostBySlug = (slug: string) => {
   return getContent().getOneBySlug(slug);
+};
+
+interface Post {
+  title: string;
+  slug: string;
+  date: string;
+  cover: string;
+  description: string;
+  category: string;
+  tags: string[];
+}
+interface Grouped {
+  [key: string]: Post[];
+}
+
+export const getPostsWithGrouped = (
+  objectKey: string,
+  valKey: "array" | "string" = "string"
+) => {
+  const content = getContent().getMetadataArray();
+
+  const groups: Grouped = {};
+
+  for (const post of content) {
+    if (valKey === "array") {
+      if (post["fields"][objectKey]) {
+        for (const val of post["fields"][objectKey]) {
+          const sVal = s(val);
+          if (!groups[sVal]) {
+            groups[sVal] = [];
+          }
+
+          const postItem: Post = {
+            title: post.fields.title,
+            slug: post.fields.slug,
+            date: post.fields.date,
+            cover: post.fields.cover,
+            description: post.fields.description,
+            category: post.fields.category,
+            tags: post.fields.tags,
+          };
+
+          groups[sVal].push(postItem);
+        }
+      }
+    }
+  }
+
+  return groups;
 };
