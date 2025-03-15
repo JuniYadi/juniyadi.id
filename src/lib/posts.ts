1dd22bd89bc1bd003e43845d48713310e5024a8a
JuniYadi <juniyadi94@gmail.com>
2025-03-16 02:01:29 +0700

    fix: Simplify post retrieval by removing redundant ordering in getAllPosts

--- src/lib/posts.ts
+++ src/lib/posts.ts
@@ -1,16 +1,18 @@
 import FusionCollection from "fusionable/FusionCollection";
 
 export const getContent = () => {
-  const collection = new FusionCollection().loadFromDir("src/contents/posts");
+  const collection = new FusionCollection()
+    .loadFromDir("src/contents/posts")
+    .orderBy("date", "desc");
   return collection;
 };
 
 export const getAllPosts = (limit?: number) => {
   if (limit) {
-    return getContent().orderBy("date", "desc").limit(limit).getItemsArray();
+    return getContent().limit(limit).getItemsArray();
   }
 
-  return getContent().orderBy("date", "desc").getItemsArray();
+  return getContent().getItemsArray();
 };
 
 export const getPostBySlug = (slug: string) => {
