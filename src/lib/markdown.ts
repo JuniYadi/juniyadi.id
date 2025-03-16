77ffdf141b40c314f1eccdac4676cb8bc769b8ff
JuniYadi <juniyadi94@gmail.com>
2025-03-16 14:53:31 +0700

    feat: Add flexible code titles and containers for enhanced markdown rendering

--- src/lib/markdown.ts
+++ src/lib/markdown.ts
@@ -3,20 +3,25 @@ import remarkEmoji from "remark-emoji";
 import rehypeSlug from "rehype-slug";
 import rehypeSanitize from "rehype-sanitize";
 import rehypeAutolinkHeadings from "rehype-autolink-headings";
-import rehypeToc from "rehype-toc";
 import rehypeHighlight from "rehype-highlight";
 import rehypeCodeLine from "rehype-highlight-code-lines";
+import remarkFlexibleCodeTitles from "remark-flexible-code-titles";
+import remarkFlexibleContainers from "remark-flexible-containers";
 
 export const MarkdownOptions = {
   mdxOptions: {
-    remarkPlugins: [remarkGfm, remarkEmoji],
+    remarkPlugins: [
+      remarkGfm,
+      remarkEmoji,
+      remarkFlexibleCodeTitles,
+      remarkFlexibleContainers,
+    ],
     rehypePlugins: [
       rehypeSanitize,
       rehypeSlug,
       rehypeAutolinkHeadings,
       rehypeHighlight,
       [rehypeCodeLine, { showLineNumbers: false }] as never,
-      rehypeToc,
     ],
   },
 };
